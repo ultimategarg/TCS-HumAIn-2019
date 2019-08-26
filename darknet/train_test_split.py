fb0b6b53e13c6addc9af37169597b288f3e802cd
garg525modi <manishgarg9694@gmail.com>
2019-08-26 09:02:00 +0530

    add darknet

---
+++ darknet/train_test_split.py
@@ -0,0 +1,21 @@
+import os
+# directory obj is in data folder of darknet which contains both images and labels
+current_dir = "obj"
+# Directory where the data will reside
+path_data = 'data/obj/'
+# Percentage of images to be used for the test set
+percentage_test = 10;
+# Create  train.txt and test.txt
+file_train = open('train.txt', 'w')  
+file_test = open('test.txt', 'w')
+# Populate train.txt and test.txt
+counter = 1  
+index_test = round(100 / percentage_test)  
+
+for i in range(2,237):
+    if counter == index_test:
+        counter = 1
+        file_test.write(path_data + "{0}".format(i) + '.jpg' + "\n")
+    else:
+        file_train.write(path_data + "{0}".format(i) + '.jpg' + "\n")
+        counter = counter + 1
